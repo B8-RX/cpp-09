c53fedc0fb25b93ba49a8ff792644be2aaa26f8f
Sofiane Sghiouar <sofiane.sghiouar@gmail.com>
2026-02-08 22:02:12 +0100

    cpp09/ex02: implement clock

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,24 +1,19 @@
 #include <algorithm>
 #include <cstddef>
+#include <ctime>
 #include <deque>
 #include <functional>
+#include <iomanip>
+#include <ios>
 #include <iostream>
 #include <limits>
 #include <sstream>
 #include <vector>
 
-int	main(int argc, char **argv) {
-	if (argc == 1)
-	{
-		std::cerr << "Error\n";
-		return (1);
-	}
-	std::vector<int>	vec;
-	std::deque<int>		deq;
-
+int	process_vec(std::vector<int>& vec, char** input, int argc) {
 	for (int i = 1; i < argc; i++)
 	{
-		std::istringstream	ss(argv[i]);
+		std::istringstream	ss(input[i]);
 		long				nl;
 		char				extra;
 		if (!(ss >> nl))
@@ -33,9 +28,50 @@ int	main(int argc, char **argv) {
 				return (std::cerr << "Error\n", 1);
 		}
 		vec.push_back(static_cast<int>(nl));
+	}
+	return (0);
+}
+
+int	process_deq(std::deque<int>& deq, char** input, int argc) {
+	for (int i = 1; i < argc; i++)
+	{
+		std::istringstream	ss(input[i]);
+		long				nl;
+		char				extra;
+		if (!(ss >> nl))
+			return (std::cerr << "Error\n", 1);
+		if (ss >> extra)
+			return (std::cerr << "Error\n", 1);
+		if (nl <= 0 || nl > std::numeric_limits<int>::max())
+			return (std::cerr << "Error\n", 1);
+		for (std::size_t i = 0; i < deq.size(); ++i)
+		{
+			if (deq[i] == (int)nl)
+				return (std::cerr << "Error\n", 1);
+		}
 		deq.push_back(static_cast<int>(nl));
 	}
-	std::cout << "Before: ";
+	return (0);
+}
+
+int	main(int argc, char **argv) {
+	if (argc == 1)
+	{
+		std::cerr << "Error\n";
+		return (1);
+	}
+	std::vector<int>	vec;
+	std::deque<int>		deq;
+	std::clock_t	t_start_vec = std::clock();
+	if (process_vec(vec, argv, argc))
+		return (1);
+	std::clock_t	t_end_vec = std::clock();
+	std::clock_t	t_start_deq = std::clock();
+	if (process_deq(deq, argv, argc))
+		return (1);
+	std::clock_t	t_end_deq = std::clock();
+
+	std::cout << "vec Before: ";
 	for (std::size_t i = 0; i < vec.size(); ++i)
 		std::cout << vec[i] << " ";
 	std::cout << "\n";
@@ -44,6 +80,20 @@ int	main(int argc, char **argv) {
 	for (std::size_t i = 0; i < vec.size(); ++i)
 		std::cout << vec[i] << " ";
 	std::cout << "\n";
+	
+
+	std::cout << "deq Before: ";
+	for (std::size_t i = 0; i < deq.size(); ++i)
+		std::cout << deq[i] << " ";
+	std::cout << "\n";
+	std::sort(deq.begin(), deq.end());
+	std::cout << "After: ";
+	for (std::size_t i = 0; i < deq.size(); ++i)
+		std::cout << deq[i] << " ";
+	std::cout << "\n";
+
+	std::cout << "vec processing time: " << std::fixed << std::setprecision(2) << 1000.0 * (t_end_vec - t_start_vec) / CLOCKS_PER_SEC << "\n";
+	std::cout << "deq processing time: " << std::fixed << std::setprecision(2) << 1000.0 * (t_end_deq - t_start_deq) / CLOCKS_PER_SEC << "\n";
 	return (0);
 }
 
